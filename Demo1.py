618d5d94399ba6fe94a9be39c6f31231f3d3ffe6
Jacob Edwards <91273401+jaxob333@users.noreply.github.com>
2024-03-14 09:18:48 -0500

    Update Demo1.py

--- Demo1.py
+++ Demo1.py
@@ -1,2 +1,7 @@
+#This line is asking the user for input for what company.
 sym = input("Enter the stock symbol of the company you wish to view: ")
+
+#This line is asking the user for input on what type of chart they wish to view.
 chart = input("Enter what type of chart would you like to view: ")
+
+
