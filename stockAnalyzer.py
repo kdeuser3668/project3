83daeecee5fb5e748cc3350facc40bda4ef974ad
Austin Barr <97968882+S1DY0M@users.noreply.github.com>
2024-03-16 12:18:45 -0500

    Add updated stockAnalyzer.py
    Get user input update

--- stockAnalyzer.py
+++ stockAnalyzer.py
@@ -1,1 +1,15 @@
+# stockAnalyzer.py
 
+from datetime import datetime
+import requests
+import pygal
+import webbrowser
+
+def get_user_input():
+    stock_symbol = input("Enter the stock symbol: ").upper()
+    chart_type = input("Enter 1 for Bar chart, 2 for Line chart: ")
+    time_series = input("Enter time series (1: Intraday, 2: Daily, 3: Weekly, 4: Monthly): ")
+    start_date = input("Enter the start date (YYYY-MM-DD): ")
+    end_date = input("Enter the end date in YYYY-MM-DD format: ")
+
+    return stock_symbol, chart_type, time_series, start_date, end_date
